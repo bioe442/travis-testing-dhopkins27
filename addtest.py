cf610913b7fc1d5a49e5eef131627f8ba1766162
Daniel Hopkins <dhopkins162@gmail.com>
2019-09-16 18:22:46 -0400

    Comment
    Comment

--- addtest.py
+++ addtest.py
@@ -1,6 +1,4 @@
 """
-  Edit the following so that it will take the two inputs and return their sum.
-  You should only need to add 1 or 2 lines to accomplish this :)
 """
-def add(a, b):
-    
+def add(a,b):
+    return(a+b)
